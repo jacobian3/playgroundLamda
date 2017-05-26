9af220b4795071505d36861aa31410cdcdbe51c2
jacobian3 <jacobian.3@gmail.com>
2017-05-26 18:38:16 -0400

    annotated study notes

--- scripts_CE9980/Session08_InputOutputExceptions.py
+++ scripts_CE9980/Session08_InputOutputExceptions.py
@@ -2,47 +2,116 @@
 
 # INDEXERROR WITH SYS.ARGV (WHEN USER PASSES NO ARGUMENT)
 # when using sys.argv, what happens when the user passes no argument?
+    # sys module
+    # program ask for items not avail
+# x
 
 # WRITING AND APPENDING TO FILES USING THE FILE OBJECT
 # When we need to open or append a file how do we do it?
+    # file write() method
+    # explicit newlines
+    # we can use "with" to avoid having to close the file
+# x
         
-# SUMMARY TASK: READING AND REDIRECTING THE STDOUT DATA STREAM
 # SUMMARY TASK: REDIRECTING THE STDOUT DATA STREAM
-# What do we call the 'output pipe' from our program to the os?
-# What three ways can we 'pipe' the output to onother program?
+# Why do we call it the 'output pipe' from our program?
+    # conduit
+    # i.e.,any print statemen
+    # redirection operator STDIN; '<' ; cat < file
+    # redireciton operator STDIN; '|' ; ls -l | xx.py (from programs)
+    # redirection operator STDOUT; '>' ; ls > file(for files)
+    # redirection operator STDOUT; '|' ; STDOUT -> STDIN(for programs)
+
+    # === write STDOUT to a file with '>' === 
+    # 
+    # $ myprog > myfile.txt
+    # 
+    # 
+    # === append STDOUT to a file with '>>' === 
+    # 
+    # $ myprogram >> myfile.txt
+    # 
+    # === pipe STDOUT to another program's STDIN with '|' ===
+    # 
+    # $ myprog | otherprog
+    # 
+    # === direct contents of a file to STDIN '<'
+    # 
+    # $ myprog < myfile.txt
+# x
 
 # SUMMARY TASK: WRITING TO STDOUT IN DIFFERENT WAYS
-# what do we call the 'input pipe' to our program (usually the keyboard)?
+# What three ways can we write to STDOUT?
+# x
+
+# SUMMARY TASK: READING AND REDIRECTING THE STDIN DATA STREAM
+# why do we call the 'input pipe' to our program?
+# In what ways do we pipe data into our program?
+# x
 
 # SUMMARY TASK: READ DIRECTORIES WITH OS.LISTDIR()
 # What are the functions employed in order to read a directory?
+    # os.listdir(path) 
 # How does os.path.join() help with this?
-# NOTE:
-# os.path.join(): Backslash on Windows and Forward Slash on OS X and Linux
-# The os.path.join() function is helpful if you need to create strings for 
-# filenames.
+    # NOTE:
+    # The os.path.join() function is helpful if you need to create strings for 
+    # filenames.
+    # os.path.sep ; returns correct seperator for  operating sys ('/') ('\')
+    # backslash on Windows and Forward Slash on OS X and Linux
+
+    # HANDLING ABSOLUTE AND RELATIVE PATHS
+    # os.path.abspath(path)
+    # os.path.isabs(path)
+    # os.path.relpath(path, start) ; <path> folder ->  ../<start> (the last '/')
+    # os.path.dirname(path)
+    # os.path.basename(path)
+    # *os.path.split() - ... 
+    # *os.path.join() - ... is helpful if y need to create strings for filenames
+# x
+
+#THE CURRENT WORKING DIRECTORY
+# How can you can get/change the current working directory as a string value?
+    # the os.getcwd() function and change it with os.chdir().
+    # *the os.chdir() changes
+# x
+
+# CREATING NEW FOLDERS WITH OS.MAKEDIRS();(ATBS, files)
+# how do you make a new directory?
+    # os.makedirs()
+# x
 
 # SUMMARY TASK: READ DIRECTORY LISTING TYPE WITH OS.PATH.ISFILE() AND 
 # OS.PATH.ISDIR()
-# What methods are used to distinguish a file from a directory?
+# What methods are used to distinguish a file from a directory? 
+    # CHECKING PATH VALIDITY
+    # os.path.exists(path) - path/file existance
+    # os.path.isfile(path)
+    # os.path.isdir(path)
+# x
 
 # SUMMARY TASK: READ FILE SIZE WITH OS.PATH.GETSIZE()
 # What do we need to do in order to read file size?
+    # os.path.getsize(path)
+# x
 
 # SUMMARY EXCEPTION: OSERROR WITH OS.LISTDIR() (AND A BAD DIRECTORY)
 # What will happen if we try to read from a directory that doesn't exist?
 # How do we handle this situation?
+# x
 
 # SUMMARY MODULE: LAUNCH AN EXTERNAL PROGRAM WITH SUBPROCESS
 # What module is imorted to create a subprocess?
 # What do subprocesses allow us to do?
-
-# FORKING CHILD PROCESSES WITH MULTIPROCESSING
-# What is forking? What situations is it used?
+    # keyword arguments 
+    # stdin= (the standard input stream: default keyboard)
+    # stdout= (standard output stream: default screen)
+    # stderr= (standard error output stream: default screen) 
+# x
 
 # SIDEBAR -- SUMMARY FUNCTION: TRAVERSE A DIRECTORY TREE WITH OS.WALK())
 # What is so 'magical' about traversing a tree with python?
 # How does the process occour?
+# x
 
 # SUMMARY TASK: READING AND REDIRECTING THE STDIN DATA STREAM
 # 3 DATA STREAMS IN UNIX / WINDOWS
@@ -53,11 +122,7 @@
 #     - may read from STDIN
 #     - may read/write to files(the disk)
 #     - may make a network connection (over socket)
-
-# STDIN (the standard input stream: default keyboard)
-# STDOUT (standard output stream: default screen)
-# STDERR (standard error output stream: default screen) 
-
+# x
 ------------------------------------Exceptions
 
 # INTRODUCTION: EXCEPTIONS
@@ -75,98 +140,3 @@
 # SUMMARY TECHNIQUE: TRAPPING MULTIPLE EXCEPTIONS
 # SUMMARY TECHNIQUE: CHAINING EXCEPT: BLOCKS
 # How do we trap multiple exeptions? What are the two methods employed?
-
------------------------------------- ATBS notes
-#THE CURRENT WORKING DIRECTORY
-# You can get the current working directory as a string value with 
-# the os.getcwd() function and change it with os.chdir().
-# *the os.chdir() changes
-
-# CREATING NEW FOLDERS WITH OS.MAKEDIRS()
-
-# HANDLING ABSOLUTE AND RELATIVE PATHS
-# os.path.abspath(path)
-# os.path.isabs(path)
-# os.path.relpath(path, start) ; <path> folder ->  ../<start> (the last '/')
-# os.path.sep ; returns correct seperator for  operating sys ('/') ('\')
-# os.path.dirname(path)
-# os.path.basename(path)
-# *os.path.split() - ... 
-# *os.path.join() - ... is helpful if y need to create strings for filenames
-
-# FINDING FILE SIZES AND FOLDER CONTENTS
-# os.path.getsize(path)
-# os.listdir(path) 
-
-# CHECKING PATH VALIDITY
-# os.path.exists(path)
-# os.path.isfile(path)
-# os.path.isdir(path)
-
-
-
-# === write STDOUT to a file with '>' === 
-# 
-# $ myprog > myfile.txt
-# 
-# 
-# === append STDOUT to a file with '>>' === 
-# 
-# $ myprogram >> myfile.txt
-# 
-# === pipe STDOUT to another program's STDIN with '|' ===
-# 
-# $ myprog | otherprog
-# 
-# === direct contents of a file to STDIN '<'
-# 
-# $ myprog < myfile.txt
-
-# @24:22
-import os
-
-for item in os.listdir('/'):
-    print('entry: ' + item)
-    if os.path.isfile(item):
-        print(os.path.getsize(item))
-# SYS.ARGV
-
-# 3 DATA STREAMS IN UNIX / WINDOWS
-# process: a running program
-#     - has a PID (process ID)
-#     - allocates memory with permission of the OS
-#     - writes to STDOUT and/or STDERR
-#     - may read from STDIN
-#     - may read/write to files(the disk)
-#     - may make a network connection (over socket)
-
-# STDIN (the standard input stream: default keyboard)
-# STDOUT (standard output stream: default screen)
-# STDERR (standard error output stream: default screen) 
-
-# === write STDOUT to a file with '>' === 
-# 
-# $ myprog > myfile.txt
-# 
-# 
-# === append STDOUT to a file with '>>' === 
-# 
-# $ myprogram >> myfile.txt
-# 
-# === pipe STDOUT to another program's STDIN with '|' ===
-# 
-# $ myprog | otherprog
-# 
-# === direct contents of a file to STDIN '<'
-# 
-# $ myprog < myfile.txt
-
-# @24:22
-import os
-
-for item in os.listdir('/'):
-    print('entry: ' + item)
-    if os.path.isfile(item):
-        print(os.path.getsize(item))
-# SYS.ARGV
-
