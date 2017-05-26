cf7d0040d2600f6784a17b1df26bd22f564c0d1a
jacobian3 <jacobian.3@gmail.com>
2017-05-26 00:37:40 -0400

    annotated problem steps for easyier study

--- scripts_CE9980/Ex08.06.py
+++ scripts_CE9980/Ex08.06.py
@@ -3,6 +3,7 @@
 import sys
 import os
 
+# STEP 1
 # validate directory name from the user at the CLI 
 try:
     #dirname = sys.argv[1]
@@ -11,22 +12,29 @@ try:
 except IndexError:
     exit("error: please provide an argument")
 
+# STEP 2
 # validate directory existance
 try:
     files = os.listdir(dirname) #assigns a list of files in given directory
 except IOError:
     exit("error: directory doesn't exist or is not readable")
 
+# STEP 3
 # loops through each list filename item
 for filename in files:
     # joins and assigns ea. filename with its respective dirname (given @ CLI)
     # this is a relpath
     full_path = os.path.join(os.path.abspath(dirname), filename)
+    # STEP 4
     # test for file/directory type
     if os.path.isfile(full_path):
         this_type = 'file'
     else:
         this_type = 'dir'
 
+    # STEP 5 
+    # set file size 
     file_size = os.path.getsize(full_path) # just gets the size of the abspath
+
+    # STEP 6 : write result
     print("{} ({}: {})".format(filename, this_type, file_size))
