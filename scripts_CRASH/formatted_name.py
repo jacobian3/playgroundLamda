f9be3124d8e2f3b88b7fea207061ef21c642ec5c
jacobian3 <jacobian.3@gmail.com>
2017-09-30 03:41:15 -0400

    update file

--- scripts_CRASH/formatted_name.py
+++ scripts_CRASH/formatted_name.py
@@ -1,9 +1,11 @@
 #@param def of function takes two parameters 
 #@return outputs neatly formatted name of a person
 #def get_formatted_name(first_name, last_name):
+# take optional arg
 def get_formatted_name(first_name, last_name, middle_name=''): # optional arg
     """Return a full name, neatly formatted"""
-    if middle_name:
+    # conditional happens only if optional argument is non empty string.
+    if middle_name: 
         full_name = first_name + ' ' + middle_name + ' ' + last_name
     else:
         full_name = first_name + ' ' + last_name
