ce1327a3fe015b70d102b3e6c4f94cdbb70651c4
jacobian3 <jacobian.3@gmail.com>
2017-10-02 17:53:33 -0400

    complete problem Ex86

---
+++ scripts_WKBK/int_ordinal.py
@@ -0,0 +1,23 @@
+def intToOrdinal(integer):
+    """
+    convert and integer between 1 and 12 to ordinal
+    """
+    my_dict = {
+            1:"first",
+            2:"second",
+            3:"third",
+            4:"fourth",
+            5:"fifth",
+            6:"sixth",
+            7:"seventh",
+            8:"eighth",
+            9:"ninth",
+            10:"tenth",
+            11:"eleventh",
+            12:"twelth"
+            }
+
+    return my_dict[integer]
+
+
+    
