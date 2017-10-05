0452c89c2b35b1eacd4a7ce76d3ff60818f62318
jacobian3 <jacobian.3@gmail.com>
2017-10-05 03:08:16 -0400

    modify attribute through method

--- scripts_CRASH/car.py
+++ scripts_CRASH/car.py
@@ -19,11 +19,21 @@ class Car():
         # make it easy to read by using a function to repeat task, when needed
         print("This car has " + str(self.odometer_reading) + " miles on it.")
 
+    def update_odometer(self, mileage):
+        """ Set the odometer reading to the given value."""
+        self.odometer_reading = mileage
+
 def main():
     # make instance of car class and store in instance variable
     my_new_car = Car('audi', 'a4', 2016)
     print(my_new_car.get_descriptive_name())
     my_new_car.read_odometer()
+
+    # ???  my_car.make = 'BMW' doesn't work as expected
+
+    my_new_car.update_odometer(23)
+    my_new_car.read_odometer()
+    
 
 if __name__ == "__main__":
     main()
