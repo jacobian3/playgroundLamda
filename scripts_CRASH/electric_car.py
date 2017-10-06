472750d735ba530b409b20b90459afe54417021d
jacobian3 <jacobian.3@gmail.com>
2017-10-06 13:42:22 -0400

    update ElectricCar class

--- scripts_CRASH/electric_car.py
+++ scripts_CRASH/electric_car.py
@@ -1,42 +1,63 @@
-"""A set of classes that can be used to represent electric cars."""
-
-from car import Car
-
-class Battery():
-    """A simple attempt to model a battery for an electric car"""
-
-    def __init__(self, battery_size=70): 
-        """Initialize the battery's attributes."""
-        self.battery_size = battery_size
-
-    def describe_battery(self):
-        """Print a statement describing the battery size"""
-        print("This car has a " + str(self.battery_size) + "-kwh battery.")
-
-    def get_range(self):
-        """Print a statement about the range this battery provides."""
-        if self.battery_size == 70:
-            range = 240
-        elif self.battery_size == 85:
-            range = 270
-
-        message = "This car can go approximately " + str(range)
-        message += " miles on a full charge."
-        print(message)
-
-
-class ElectricCar(Car):
-    """Represent aspects of a car, specific to electric vehicles"""
+class Car():
+    """ A simple attempt to represent a car. """
 
     def __init__(self, make, model, year):
-        """
-        Initialize attributes of the parent class
-        Then initialize attributes specific to an electric car.
-        """
-        super().__init__(make, model, year)
-        self.battery = Battery()
+        """ Initialize attributes to describe a car. """
+        self.make = make # store parameters in instance name space
+        self.model = model # instance namespace => attributes
+        self.year = year
+        self.odometer_reading = 0 # set odometer in attribute so no parameter
 
-    # used to illustrate method overrides
-    def fill_gas_tank(self, level):
-        """Electric cars don't have gas tanks."""
-        print("This car doesn't need a gas tank!")
+    def get_descriptive_name(self):
+        """ Return a neatly formatted descriptive name. """
+        # store attributes values in a variable for convenience
+        long_name = str(self.year) + ' ' + self.make + ' ' + self.model 
+        return long_name.title()
+
+    def read_odometer(self):
+        """ Print a statement showing the car's mileage."""
+        # make it easy to read by using a function to repeat task, when needed
+        print("This car has " + str(self.odometer_reading) + " miles on it.")
+
+    def update_odometer(self, mileage):
+        """ 
+        Set the odometer reading to the given value.
+        Reject the change, if it attempts to roll the odometer back. 
+        """
+        if mileage >= self.odometer_reading:
+            self.odometer_reading = mileage
+        else:
+            print("You can't roll back an odometer!")
+
+    def increment_odometer(self, miles):
+        """ Add the given amount to the odometer reading. """
+        self.odometer_reading += miles
+
+class ElectricCar(Car):
+    """ Represent aspects of a car, specific to electric vehicles."""
+    # Initialize the new class and accept parameters passed from the intance
+    # Initialize all new attibutes of child class
+    def __init__(self, make, model, year):
+        """ 
+        Initialize attributes of the parent class.
+        Then initialize attributes specific to an electric car
+        """
+        # connect the parrent class to the child class and access all attribute
+        # data
+        super().__init__(make, model, year)
+        # add new attributes to differentiate parent child relationship
+        self.battery_size = 70
+
+    def describe_battery(self):
+        """ Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+def main():
+    """ run function calls """
+    my_tesla = ElectricCar('tesla','model s', 2016)
+    print(my_tesla.get_descriptive_name())
+    my_tesla.describe_battery()
+
+if __name__ == "__main__": # gateway to module access
+    main()
+
