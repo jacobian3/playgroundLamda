686983e63acb4a4cef1b210eef1dbbf4b51f2e39
jacobian3 <jacobian.3@gmail.com>
2017-10-06 14:21:00 -0400

    update ElectricCar class range for battery

--- scripts_CRASH/electric_car.py
+++ scripts_CRASH/electric_car.py
@@ -33,6 +33,7 @@ class Car():
         """ Add the given amount to the odometer reading. """
         self.odometer_reading += miles
 
+
 class ElectricCar(Car):
     """ Represent aspects of a car, specific to electric vehicles."""
     # Initialize the new class and accept parameters passed from the intance
@@ -46,17 +47,49 @@ class ElectricCar(Car):
         # data
         super().__init__(make, model, year)
         # add new attributes to differentiate parent child relationship
-        self.battery_size = 70
+        self.battery = Battery() # call Battery class to get battery
+        # information
+        # or said another way: create an instance of the Batery class and set 
+        # self to 'battery'
 
     def describe_battery(self):
         """ Print a statement describing the battery size."""
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
+
+class Battery():
+    """A simple attempt to model a battery. """
+    # set default battery to 70 to facilitate later function calls out of class
+    def __init__(self, battery_size=70):
+        """ Initialize battery attributes. """
+        self.battery_size = battery_size
+
+    def describe_battery(self):
+        """ Print a statement describing the battery size."""
+        print("This car has a " + str(self.battery_size) + "-kWh battery.")
+
+    def get_range(self):
+        """ Print a statement about the range of of this battery """
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
+
 def main():
     """ run function calls """
     my_tesla = ElectricCar('tesla','model s', 2016)
     print(my_tesla.get_descriptive_name())
-    my_tesla.describe_battery()
+
+    # call for battery size from Battery() instead of from function defined 
+    # inside of ElectricCar()
+    #my_tesla.describe_battery()
+    my_tesla.battery.describe_battery()
+    my_tesla.battery.get_range()
+
 
 if __name__ == "__main__": # gateway to module access
     main()
