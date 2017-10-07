f7ec1f7d8a5c5d6b09f88b21829da27a4626caba
jacobian3 <jacobian.3@gmail.com>
2017-10-06 20:08:22 -0400

    make car.py into module

--- scripts_CRASH/car.py
+++ scripts_CRASH/car.py
@@ -33,35 +33,4 @@ class Car():
         """ Add the given amount to the odometer reading. """
         self.odometer_reading += miles
 
-def main():
-    # make instance of car class and store in instance variable
-    my_new_car = Car('audi', 'a4', 2016)
-    print(my_new_car.get_descriptive_name())
-    my_new_car.read_odometer()
-
-    # modify attribute through method
-    my_new_car.update_odometer(20000)
-    my_new_car.read_odometer()
-
-    # modify attribute through method with conditions
-    my_new_car.update_odometer(30) # 'roll back odometer')
-    my_new_car.read_odometer()
-
-    # add miles to existing odometer 
-    my_new_car.increment_odometer(300)
-    my_new_car.read_odometer()
-
-    # if new used_car instance, add miles 
-    # and increment the odometer 
-    my_used = Car("BMW", "3Series", 2006)
-    print(my_used.get_descriptive_name())
-
-    my_used.increment_odometer(130500)
-    my_used.read_odometer()
-    
-    my_used.increment_odometer(100)
-    my_used.read_odometer()
-
-if __name__ == "__main__":
-    main()
 
--- scripts_CRASH/electric_car.py
+++ scripts_CRASH/electric_car.py
@@ -1,39 +1,3 @@
-class Car():
-    """ A simple attempt to represent a car. """
-
-    def __init__(self, make, model, year):
-        """ Initialize attributes to describe a car. """
-        self.make = make # store parameters in instance name space
-        self.model = model # instance namespace => attributes
-        self.year = year
-        self.odometer_reading = 0 # set odometer in attribute so no parameter
-
-    def get_descriptive_name(self):
-        """ Return a neatly formatted descriptive name. """
-        # store attributes values in a variable for convenience
-        long_name = str(self.year) + ' ' + self.make + ' ' + self.model 
-        return long_name.title()
-
-    def read_odometer(self):
-        """ Print a statement showing the car's mileage."""
-        # make it easy to read by using a function to repeat task, when needed
-        print("This car has " + str(self.odometer_reading) + " miles on it.")
-
-    def update_odometer(self, mileage):
-        """ 
-        Set the odometer reading to the given value.
-        Reject the change, if it attempts to roll the odometer back. 
-        """
-        if mileage >= self.odometer_reading:
-            self.odometer_reading = mileage
-        else:
-            print("You can't roll back an odometer!")
-
-    def increment_odometer(self, miles):
-        """ Add the given amount to the odometer reading. """
-        self.odometer_reading += miles
-
-
 class ElectricCar(Car):
     """ Represent aspects of a car, specific to electric vehicles."""
     # Initialize the new class and accept parameters passed from the intance
@@ -69,7 +33,7 @@ class Battery():
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
     def get_range(self):
-        """ Print a statement about the range of of this battery """
+        """ Print a statement about the range of of this battery."""
         if self.battery_size == 70:
             range = 240
         elif self.battery_size == 85:
