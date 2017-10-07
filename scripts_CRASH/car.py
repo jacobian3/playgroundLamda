421e98d94ebf421a6346b44d4e520676606951a4
jacobian3 <jacobian.3@gmail.com>
2017-10-06 20:32:22 -0400

    update my_cars.py to inport multiple classes.

--- scripts_CRASH/car.py
+++ scripts_CRASH/car.py
@@ -51,9 +51,25 @@ class ElectricCar(Car):
         # or said another way: create an instance of the Batery class and set 
         # self to 'battery'
 
+class Battery():
+    """A simple attempt to model a battery. """
+    # set default battery to 70 to facilitate later function calls out of class
+    def __init__(self, battery_size=70):
+        """ Initialize battery attributes. """
+        self.battery_size = battery_size
+
     def describe_battery(self):
         """ Print a statement describing the battery size."""
         print("This car has a " + str(self.battery_size) + "-kWh battery.")
 
+    def get_range(self):
+        """ Print a statement about the range of of this battery."""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
 
+        message = "This car can go approximately " + str(range)
+        message += " miles on a full charge."
+        print(message)
 
