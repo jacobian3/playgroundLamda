6cba39104725d393306adbb306cf87f2d85997d7
jacobian3 <jacobian.3@gmail.com>
2017-10-11 02:11:53 -0400

    complete ex9.6ice_cream_stan.py

--- scripts_CRASH/ex9.6ice_cream_stan.py
+++ scripts_CRASH/ex9.6ice_cream_stan.py
@@ -26,28 +26,32 @@ class Restaurant():
 
 
 class IceCreamStand(Restaurant):
-    """ a simple inheritance example of specific restaurant. """
-    def __init__(self, name, cuisine, *flavors):
-        """initialize the restaurant's attributes """
-        self.flavors = flavors
-        super.__init__(name, cuisine)
-        """ initiale attributes of parent """  
+    """ simple model of an ice cream restaurant """
+
+    def __init__(self, name, cuisine_type="Ice Cream"): # default value used
+        """ init the stand """
+        # connect parent and child class using py3.6 syntax 
+        super().__init__(name, cuisine_type)
+        self.flavors = []
 
     def display_flavors(self):
-        print("{} has {} flavors.".format(self.name, len(self.flavors)))
+        """ Display the flavors """
+        print("\n These flavors are available: ")
         for flavor in self.flavors:
-            print(" - " + flavor)
+            print("- " + flavor)
 
 if __name__ == '__main__':
+
     fav_joint = Restaurant('Chick-fil-A','chick-burger')
     fav_joint.describe_restaurant()
 
-    restaurant = Restaurant('applebees','dinner cuisine')
+    restaurant = Restaurant('applebees','dinner cuisine_type')
     restaurant.set_number_served(5)
     restaurant.describe_restaurant()
 
     # instance of IceCreamStand
-    my_store = IceCreamStand('troy','ice cream parlor','vanilla','chocolate'
-                            'neopolitan')
+    my_store = IceCreamStand('TS&C')
+    my_store.flavors = ['vanilla','chocolate','blackberry']
+
     my_store.describe_restaurant()
     my_store.display_flavors()
